c81569fe0a2793f79b97770eed58d62214120d18
Matheus Cavalcante <cavalcantemat43@gmail.com>
2023-10-05 00:19:59 -0300

    feat: create User components

---
+++ src/main/java/com/picpay/Controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.picpay.Controllers;
+
+import com.picpay.dtos.UserDTO;
+import com.picpay.services.UserService;
+import domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(UserDTO data) {
+        User newUser = userService.createUser(data);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
